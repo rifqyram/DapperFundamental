5f39145025ac091781affb2d185980729b234613
Rifqi Ramadhan <rifqyomp@gmail.com>
2023-01-11 07:04:53 +0700

    commit lambda & linq

--- DapperFundamental/Program.cs
+++ DapperFundamental/Program.cs
@@ -1,142 +1,164 @@
-﻿using System.Collections;
-using System.Data.SqlClient;
-using Dapper;
-using DapperFundamental;
+﻿namespace DapperFundamental;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         /*
-         * Dapper adalah ORM (micro ORM) yang memungkinkan kita untuk mengakses data lebih cepat dan
-         * memapping object
+         * LINQ -> Language-Integrated Query
+         * LINQ ini mendukung banyak data source:
+         * - In Memory Object (List, Array)
+         * - ADO.NET (DataSet)
+         * - Entity Framework
+         * - SQL Server
+         * - XML Document
+         * - Data Source lain-lain
          *
-         * Keunggulan Dapper
-         * - Dapper ringan dan cepat
-         * - Dapper ini mudah digunakan dan object mapping yang powerful
-         * - Dapper mendukung yang namanya parameterized queries yang membantu kita untuk menghindari yang namanya
-         *  SQL Injection
-         *
-         * Tujuan utama Dapper me-mapping query ke object
-         *
-         * Dapper RDBMS (Provider): SQL Server, Oracle, PostgreSQL, MySQL
-         *
-         *
-         * Extensions Method: Membuat function/method didalam suatu class tanpa merubah isi dari class tersebut
+         * Linq Providers
+         * - LINQ to Objects
+         * - LINQ to Entities
+         * - LINQ to XML
+         * - LINQ to SQL
+         * - LINQ to DataSet
+         * dll
          */
 
         /*
-         * 4 Method exetension IDBConnection:
-         * - Query (DQL)
-         * - Execute (DDL, DML)
-         * - ExecuteScalar (DML, DQL) - Mengembalikan satu record dan satu column
-         * - ExecuteReader (DQL)
+         * 3 hal untuk membuat query LINQ
+         * - Data Source (SQL, In Memory Object, XML)
+         * - Query (SELECT blablabla)
+         * - Execution of the query
+         *
+         * Kombinasi Query yang kita perlu ketahui:
+         * - Query Syntax
+         * - Method Syntax
+         * - Mixed Syntax
          */
 
-        // Implementasi dari IDBConnection
-        using var connection =
-            new SqlConnection("server=localhost,1433;user=sa;password=Password123;database=enigma_shop");
-        DefaultTypeMap.MatchNamesWithUnderscores = true;
-
-        // Using Dapper
-
-        // var sql = @"
-        //     CREATE TABLE m_product
-        //     (
-        //        id int identity primary key,
-        //        product_name varchar(100),
-        //        product_price bigint,
-        //        stock int,
-        //     )";
-        // var sql = "INSERT INTO m_product (product_name, product_price, stock) VALUES ('Sepatu', 250000, 10)";
-        // connection.Execute(sql);
-
-        /*Native ADO.NET*/
-        // connection.Open();
-        // var command = new SqlCommand(sql, connection);
-        // command.ExecuteNonQuery();
-
-        // var sql = "SELECT * FROM m_product";
-        // IEnumerable -> LINQ (Bahasa Query tapi di pakai di C#)
         /*
-        var products = connection.Query<Product>(sql).ToList();
-        foreach (var product in products)
+         * Query Syntax
+         * from object in datasource // inisialisasi
+         * where condition // kondisi
+         * select object // seleksi
+         */
+
+        // numbers -> datasource (in memory objects)
+        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var filteredNumber =
+            from number in numbers //inisialisasi
+            where number > 5 // kondisi
+            orderby number descending
+            select number; // seleksi
+
+        /*
+         * Method Syntax
+         * Datasource.ConditionMethod().SelectionMethod()
+         * inisialisasi -- kondisi   --   seleksi
+         */
+
+        var evenNumbers = numbers.Where(number => number % 2 == 0).OrderByDescending(i => i);
+
+        var products = new List<Product>
         {
-            Console.WriteLine(product);
-        }
-        */
-
-        /*
-         * Menampilkan Single Data
-         * - QuerySingle - Dynamic              = Mengembalikan satu data berupa dynamic - Exception saat query kosong atau data bisa lebih dari satu
-         * - QuerySingle<T>
-         * - QuerySingleOrDefault - Dynamic     = Mengembalikan satu data berupa dynamic - Exception saat query lebih dari satu data, kalau query kosong akan return null
-         * - QuerySingleOrDefault<T>
-         * - QueryFirst - Dynamic               = Mengembalikan satu data berupa dynamic - Exception saat query itu kosong
-         * - QueryFirst<T>
-         * - QueryFirstOrDefault - Dynamic      = Mengembalikan satu data berupa dynamic - tidak mengembalikan exception apapun, kalau query kosong akan return null
-         * - QueryFirstOrDefault<T>
-         * 
-        var sql = "SELECT * FROM m_product WHERE id = 1";
-        var product = connection.QueryFirstOrDefault<Product>(sql);
-        Console.WriteLine(product);
-         */
-
-        /* Execute Scalar
-         var sql = "SELECT product_name FROM m_product";
-        var count = connection.ExecuteScalar<string>(sql);
-        Console.WriteLine(count);*/
-
-        /*
-         * Execute Reader
-          var sql = "SELECT * FROM m_product";
-        var reader = connection.ExecuteReader(sql);
-
-        while (reader.Read())
-        {
-         Console.WriteLine(new Product
-         {
-          Id = Convert.ToInt32(reader["id"]),
-          ProductName = reader["product_name"].ToString(),
-          ProductPrice = Convert.ToInt64(reader["product_price"]),
-          Stock = Convert.ToInt32(reader["stock"])
-         });
-        }
-         */
-
-        /*
-         * Parameterize Dapper - Sql Server itu menggunakan @
-         * - Anonymous Parameters
-         * - Dynamic Parameters
-         * var product = new Product
-        {
-         ProductName = "Tas",
-         ProductPrice = 100000,
-         Stock = 15
+            new() { Id = 1, ProductName = "Hoodie", ProductPrice = 100000, Stock = 10 },
+            new() { Id = 2, ProductName = "Kemeja", ProductPrice = 150000, Stock = 10 },
+            new() { Id = 3, ProductName = "Celana", ProductPrice = 100000, Stock = 10 },
+            new() { Id = 4, ProductName = "Sepatu", ProductPrice = 300000, Stock = 10 },
         };
 
-        // Anonymous Parameter with Anonymous Object
-        var productParams = new
-        {
-         productName = product.ProductName,
-         productPrice = product.ProductPrice,
-         stock = product.Stock
-        };
+        var querySyntaxProducts =
+            from product in products
+            orderby product.ProductName
+            select product.ProductName;
 
-        // Dynamic Parameters
-        var dynamicParameters = new DynamicParameters(product);
+        var methodSyntaxProducts =
+            products.Where(product => product.ProductPrice > 100000).Select(product => product.ProductName);
 
-        var sql = "INSERT INTO m_product (product_name, product_price, stock) VALUES (@productName, @productPrice, @stock)";
-        connection.Execute(sql, product);
+        // pagination menggunakan linq
+        var page = 2;
+        var size = 2;
+
+        var productsPage = products.Page(page, size);
+
+        // Console.WriteLine($"Page {page}");
+        // foreach (var product in productsPage)
+        // {
+        //     Console.WriteLine(product);
+        // }
+
+        /*
+         * Mixed Syntax
          */
 
-        var sql = "SELECT * FROM m_product WHERE product_price > @productPrice";
-        var products = connection.Query<Product>(sql, new
-        {
-            productPrice = 100000
-        }).ToList();
-        
-        products.ForEach(Console.WriteLine);
+        // var filteredProducts =
+        //     (from product in products
+        //         where product.ProductPrice is > 100000 and < 2500000
+        //         select product).OrderByDescending(p => p.ProductName);
+        //
+        // foreach (var i in filteredProducts)
+        // {
+        //     Console.WriteLine(i);
+        // }
         
     }
 }
+
+public static class MyExtensionMethod
+{
+    public static int Plus(this int value, int parameter)
+    {
+        return value + parameter;
+    }
+
+    public static IEnumerable<T> Page<T>(this IEnumerable<T> dataSource, int page, int size)
+    {
+        return dataSource.Skip((page - 1) * size).Take(size);
+    } 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*/*
+         * Lambda Expression
+         * Didefinisikan dalam bahasa pemrograman adalah anonymous function, yang artinya method/function tanpa nama
+         *
+         * 2 Tipe Lambda Expression yang digunakan di C#:
+         * - Lambda Expression: dimana body nya sebaga expression
+         * - Statement Lambda: dimana memiliki block code sebagai body nya.
+         #1#
+
+        // Anonymous function / Lambda Exression
+        var square = (int x) => x * x;
+        Console.WriteLine(square(10));
+
+        var sayHello = (Action callback) =>
+        {
+            Console.WriteLine("Hello ini lambda");
+            Console.WriteLine("Hello Dunia");
+            callback();
+        };
+        
+        var callback = () =>
+        {
+            Console.WriteLine("Halo ini adalah callback function");
+        };
+        
+        // sayHello(callback);
+        sayHello(() =>
+        {
+            Console.WriteLine("Halo ini adalah callback anonymous function");
+        });*/
